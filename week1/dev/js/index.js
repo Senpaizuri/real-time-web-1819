9302ae166d80ae2b0f7014b97a2401679da52c87
Senpaizuri <mjsleebos@gmail.com>
2019-04-09 14:29:16 +0200

    init repo

---
+++ week1/dev/js/index.js
@@ -0,0 +1,71 @@
+(()=>{
+    const 
+        socket = io(),
+        canvas = document.querySelector("canvas"),
+        config = {x:canvas.width,y:canvas.height},
+        ctx = canvas.getContext("2d"),
+        hexToRgb = (hex)=>{
+            let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            } : null
+        },
+        app = ()=>{
+            canvas.addEventListener("mousemove",(e)=>{
+                const 
+                    pos = {
+                        "x":Math.trunc((e.layerX/canvas.clientHeight)*config.x),
+                        "y":Math.trunc((e.layerY/canvas.clientWidth)*config.y)
+                    },
+                    pixel = ctx.createImageData(1,1),
+                    color = hexToRgb(document.querySelector("[type=color]").value)
+                
+                pixel.data[0] = color.r  
+                pixel.data[1] = color.g  
+                pixel.data[2] = color.b
+                pixel.data[3] = 255
+        
+                socket.emit("pixel update",{
+                    pos,
+                    "pixel":{
+                        r:pixel.data[0],
+                        g:pixel.data[1],
+                        b:pixel.data[2],
+                        a:pixel.data[3]
+                    }
+                })
+                ctx.putImageData(pixel,pos.x,pos.y)
+            })
+        
+            socket.on("pixel update",(update)=>{
+                const 
+                    pos = update.pos,
+                    pixel = ctx.createImageData(1,1)
+                
+                pixel.data[0] = update.pixel.r
+                pixel.data[1] = update.pixel.g
+                pixel.data[2] = update.pixel.b
+                pixel.data[3] = update.pixel.a
+        
+                ctx.putImageData(pixel,pos.x,pos.y)
+            })
+        }
+
+    const form = document.querySelector("form")
+
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        const name = document.querySelector("#username").value
+        socket.emit("user registration",{user:name,color:document.querySelector("[type=color]").value})
+        return false
+    })
+
+    socket.on("user registration",(e)=>{
+        document.querySelector("#users ul").innerHTML += `<li>${e}</li>`
+        app()
+        form.style.setProperty("display","none")
+    })
+
+})()
---
+++ week1/server.js
@@ -0,0 +1,57 @@
+const
+    express = require("express"),
+    app = express(),
+    http= require("http").Server(app),
+    ejs = require("ejs"),
+    compression = require("compression"),
+    io = require("socket.io")(http),
+    port = process.env.PORT || 5000
+
+let users = []
+let check = (str,arr)=>{
+    let q = false
+    arr.forEach((el,i) => {
+        if(arr[i].user == str){
+            q = true
+        } else{
+            q = false
+        }
+    })
+    return q
+}
+
+app.use(compression())
+app.use(express.static("src"))
+app.set("view engine","ejs")
+
+app.get("/",(req,res)=>{
+    res.status(200).render("home/home.ejs")
+})
+
+io.on("connection",(ws)=>{
+    console.log("a user connected")
+    ws.on("disconnect",(ws)=>{
+        console.log("a user disconnected")
+    })
+    ws.on("pixel update",(update)=>{
+        // console.log(update)
+        io.emit("pixel update",update)
+    })
+    ws.on("user registration",(update)=>{
+        let user = update.user
+        if(!check(user,users)){
+            users.push({
+                user:user
+            })
+            console.log(`user ${user} registered`)
+            io.emit("user registration",`<span style="color:${update.color}">${user}</span> has joined`)
+        } else{
+            console.log(`user ${user} already registered`)
+            io.emit("user registration",`<span style="color:${update.color}">${user}</span> has joined`)
+        }
+    })
+})
+
+http.listen(port,(a,b)=>{
+    console.log(`Active on port ${port}`)
+})
